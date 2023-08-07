b3f6117b6cbb8491d0fed4fab97c9addff544663
aaa <es955126@gmail.com>
2023-08-07 17:00:28 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HWAjax.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
